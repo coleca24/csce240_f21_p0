b61ce88087b92b2a925df738f2062bd08c196847
Casey Cole <coleca@email.sc.edu>
2021-08-12 12:46:43 -0400

    Changed helloworld.cpp file for commit 1

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,11 +1,14 @@
-// Copyright 2021 Casey Anne Cole
+// Author: <Enter name here> 
+// Email: <Enter email here> 
+// Section #: <Enter section here>
+// Copyright 2021 <Enter Name Here>
+// Modified from template code @coleca24 GitHub
 #include <gtest/gtest.h>
 #include <iostream>
 
+// DO NOT MODIFY START
 std::string genHelloWorld();
 int myAdd(int, int);
-
-
 
 TEST(HelloTest, correctStringCreated) {
      EXPECT_EQ(genHelloWorld(), "Hello World!");
@@ -24,10 +27,14 @@ int main(int argc, char** argv) {
      return RUN_ALL_TESTS();
 }
 
+// DO NOT MODIFY END
+
+// TODO(student) Make this function return (exactly) the string "Hello World!"
 std::string genHelloWorld() {
-     return "Hello World!";
+     return "";
 }
 
+// TODO(student) Make this function return the sum of the two numbers passed in
 int myAdd(int num1, int num2) {
-     return num1 + num2;
+     return 0;
 }
